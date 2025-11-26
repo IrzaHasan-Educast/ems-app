bd698bb9a832cd3e6ad4dc6b3f5ccadefa2fb83a
Irziii-Hasan <irzahasan07@gmail.com>
2025-11-26 17:11:42 +0500

    Use config.properties for DB credentials and CORS origin

--- src/main/java/com/educast/ems/config/WebConfig.java
+++ src/main/java/com/educast/ems/config/WebConfig.java
@@ -1,6 +1,7 @@
 // 📁 src/main/java/com/educast/ems/config/WebConfig.java
 package com.educast.ems.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,13 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+	@Value("${frontend.url")
+	private String frontendUrl;
+	
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // your React app
+                        .allowedOrigins(frontendUrl) // your React app
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
